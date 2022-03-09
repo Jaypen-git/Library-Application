9e067a6a101c27ceb356a69f3708b47c9f9afd36
Jeremy Penaredondo <jeremypenaredondo97@gmail.com>
2022-03-09 15:38:51 -0500

    refactored to be modular

--- script.js
+++ script.js
@@ -1,124 +1,130 @@
-// parse turns a returned string into an array
-const myLibrary = JSON.parse(localStorage.getItem('myLibrary')) || [];
-// variable for setting IDs
-let a = 0;
-// DOM queries and general functions
-const controller = {
-    tbody: document.querySelector('tbody'),
-    displayButton: document.querySelector('#displayButton'),
-    submitButton: document.querySelector('#submit'),
-    form: document.querySelector('.bookForm'),
-    title: document.querySelector('#title'),
-    author: document.querySelector('#author'),
-    pages: document.querySelector('#pages'),
-    status: document.querySelector('#status'),
-    createButton: (name, num) => {
-        let td = document.createElement('td');
-        let button = document.createElement('button');
-        button.setAttribute('id', num);
-        button.innerText = name;
-        td.appendChild(button);
-        return td;
-    },
-    addBook(bookID, bookTitle, bookAuthor, bookPages, bookStatus){
-        let newBook = new book(bookID, bookTitle, bookAuthor, bookPages, bookStatus);
-        myLibrary.push(newBook);
-        localStorage.setItem('myLibrary', JSON.stringify(myLibrary)); // convert the array into a string
-        a++;       
-    },
-    deleteBook: (e) => {
-        let target = e.target; // e = event object and target is whichever node had the event
-        let item = myLibrary.find(({bookID}) => bookID === parseInt(target.id)); // find method returns first element whose object key bookID matches the buttons id
-        let index = myLibrary.indexOf(item);
-        if (index !== -1){
-            myLibrary.splice(index, 1);
+(function(){
+    class book {
+        constructor(bookID, title, author, pages, status){
+            this.bookID = bookID; // this lets me accurately target a book in an array
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.status = status;
         }
-        let buttonContainer = target.parentNode.parentNode; // this will target the tr element. the buttons parent is the td element
-        buttonContainer.remove();
-        localStorage.setItem('myLibrary', JSON.stringify(myLibrary)); // push changes to local storage
-    },
-    changeStatus: (e) => {
-        let element = e.target;
-        let target = parseInt(element.id);
-        let libraryItem = myLibrary[target];
-        if (libraryItem.status === 'Unread'){
-            libraryItem.status = 'Read';
-            element.innerText = 'Read';
-        } else {
-            libraryItem.status = 'Unread';
-            element.innerText = 'Unread';
+    }
+
+    const library = {
+        myLibrary:JSON.parse(localStorage.getItem('myLibrary')) || [], // parse will convert the returned string into an array
+        addBook: (bookID, bookTitle, bookAuthor, bookPages, bookStatus) => {
+            bookID = a;
+            let keys = [bookID, bookTitle, bookAuthor, bookPages, bookStatus];
+            tr = page.create_tr();
+            tr.setAttribute('id', a);
+            for (let i = 1; i < keys.length; i++){ // start on key 1 to not print the books id
+                let val = keys[i];
+                if (i === 4){ // on the 4th item, the status, run this code
+                    statusButton = page.create_statusButton(bookStatus);
+                    statusButton.addEventListener('click', library.changeStatus);
+                    tr.appendChild(statusButton);
+                } else {
+                    td = page.create_td();
+                    td.innerText = val;
+                    tr.appendChild(td);
+                }
+            };
+            deleteButton = page.create_deleteButton();
+            deleteButton.addEventListener('click', library.deleteBook); // if you add parenthesis on function, it will automatically call
+            tr.appendChild(deleteButton);
+            page.tbody.appendChild(tr);
+            let newBook = new book(bookID, bookTitle, bookAuthor, bookPages, bookStatus);
+            library.myLibrary.push(newBook);
+            localStorage.setItem('myLibrary', JSON.stringify(library.myLibrary)); // convert the array into a string
+        },
+        deleteBook: (e) => {
+            let target = e.target; // e = event object and target is whichever node had the event
+            let item = library.myLibrary.find(({bookID}) => bookID === parseInt(target.id)); // find method returns first element whose object key bookID matches the buttons id
+            let index = library.myLibrary.indexOf(item);
+            if (index !== -1){
+                library.myLibrary.splice(index, 1);
+            }
+            let buttonContainer = target.parentNode.parentNode; // this will target the tr element. the buttons parent is the td element
+            buttonContainer.remove();
+            localStorage.setItem('myLibrary', JSON.stringify(library.myLibrary)); // push changes to local storage
+        },
+        changeStatus: (e) => {
+            let element = e.target;
+            let target = parseInt(element.id);
+            let libraryItem = library.myLibrary[target];
+            if (libraryItem.status === 'Unread'){
+                libraryItem.status = 'Read';
+                element.innerText = 'Read';
+            } else {
+                libraryItem.status = 'Unread';
+                element.innerText = 'Unread';
+            }
+            localStorage.setItem('myLibrary', JSON.stringify(library.myLibrary)); 
         }
-        localStorage.setItem('myLibrary', JSON.stringify(myLibrary)); 
-    },
-    displayBook: (bookID, bookTitle, bookAuthor, bookPages, bookStatus) => {
-        bookID = a;
-        let keys = [bookID, bookTitle, bookAuthor, bookPages, bookStatus];
-        let tr = document.createElement('tr');
-        tr.setAttribute('id', a);
-        for (let i = 1; i < keys.length; i++){ // start on key 1 to not print the books id
-            let val = keys[i];
-            if (i === 4){ // on the 4th item, the status, run this code
-                let statusButton = controller.createButton(val, a);
-                statusButton.firstChild.addEventListener('click', this.changeStatus);
+    }
+
+    let a = 0; // id value
+    let page = {
+        tbody: document.querySelector('tbody'),
+        submitButton: document.querySelector('#submit'),
+        title: document.querySelector('#title'),
+        author: document.querySelector('#author'),
+        pages: document.querySelector('#pages'), //parseInt converts a string number into an integer
+        status: document.querySelector('#status'),
+        create_tr: () => {
+            let tr = document.createElement('tr');
+            return tr;
+        },
+        create_td: () => {
+            let td = document.createElement('td');
+            return td;
+        },
+        create_deleteButton: () => {
+            let deleteButtoncontainer = page.create_td();
+            let deleteButton = document.createElement('button');
+            deleteButton.setAttribute('id', a); // the id corresponds to the books id
+            deleteButton.innerText = 'Delete';
+            deleteButtoncontainer.appendChild(deleteButton);
+            return deleteButtoncontainer;
+        },
+        create_statusButton: (status) => {
+            let statusButtoncontainer = page.create_td();
+            statusButton = document.createElement('button');
+            statusButton.setAttribute('id', a);
+            statusButton.innerText = status;
+            statusButtoncontainer.appendChild(statusButton);
+            return statusButtoncontainer;
+        }
+    }
+
+    library.myLibrary.forEach(function(book){
+        let keys = Array.from(Object.keys(book));
+        tr = page.create_tr();
+        for (let i = 1; i < keys.length; i++){
+            let val = book[keys[i]];
+            if (i === 4){
+                statusButton = page.create_statusButton(val);
+                statusButton.addEventListener('click', library.changeStatus);
                 tr.appendChild(statusButton);
             } else {
-                let td = document.createElement('td');
+                td = page.create_td();
                 td.innerText = val;
                 tr.appendChild(td);
             }
         }
-        let deleteButton = controller.createButton('Delete', a);
-        deleteButton.addEventListener('click', this.deleteBook);
+        deleteButton = page.create_deleteButton();
+        deleteButton.addEventListener('click', library.deleteBook);
+        tr.setAttribute('id', book.bookID);
         tr.appendChild(deleteButton);
-        controller.tbody.appendChild(tr);
-        controller.addBook(bookID, bookTitle, bookAuthor, bookPages, bookStatus);
-    }
-}
-// book object constructor
-class book {
-    constructor(bookID, title, author, pages, status){
-        this.bookID = bookID;
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.status = status;
-    }
-}
-
-myLibrary.forEach(function(book){
-    let keys = Array.from(Object.keys(book));
-    tr = document.createElement('tr');
-    for (let i = 1; i < keys.length; i++){
-        let val = book[keys[i]];
-        if (i === 4){
-            let statusButton = controller.createButton(val, a);
-            statusButton.firstChild.addEventListener('click', controller.changeStatus);
-            tr.appendChild(statusButton);
-        } else {
-            let td = document.createElement('td');
-            td.innerText = val;
-            tr.appendChild(td);
-        }
-    }
-    let deleteButton = controller.createButton('Delete', a);
-    deleteButton.firstChild.addEventListener('click', controller.deleteBook);
-    tr.setAttribute('id', a);
-    tr.appendChild(deleteButton);
-    controller.tbody.appendChild(tr);
-    a++;
-})
-
-controller.submitButton.addEventListener('click', function(){
-    let title = controller.title.value;
-    let author = controller.author.value;
-    let pages = parseInt(controller.pages.value); //parseInt converts a string number into an integer
-    let status = controller.status.value;
-    let newBook =  new book(title, author, pages, status);
-    if (isNaN(pages)){ //checks if user inputted anything but a number
-        console.log('uh-oh');
-    } else {
-        controller.addBook(a, title, author, pages, status);
-        controller.displayBook(a, title, author, pages, status);
+        page.tbody.appendChild(tr);
         a++;
-    }
-})
+    })
+
+    page.submitButton.addEventListener('click', function(){
+        // parseInt converts a string number into an integer
+        if (isNaN(page.pages.value)){ //checks if user inputted anything but a number
+            console.log('uh-oh');
+        } else {
+            library.addBook(a, page.title.value, page.author.value, parseInt(page.pages.value), page.status.value);
+        }
+    })
+})();
