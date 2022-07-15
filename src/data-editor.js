a3aef2391389477b97f2a2c692ddf65d2200aac4
Jeremy Penaredondo <jeremypenaredondo97@gmail.com>
2022-07-15 13:28:00 -0400

    fixes typo in data-editor

--- src/data-editor.js
+++ src/data-editor.js
@@ -1,15 +1,15 @@
 const Library = {
-    myLibrary: JSON.parse(localStorage.getItem('myLibrary')) || [], // parse will convert the returned string into an array
+    myLibrary: JSON.parse(localStorage.getItem('myLibrary')) || new Set(), // parse will convert the returned string into an array
     addBook: (bookID, bookTitle, bookAuthor, bookPages, bookStatus) => {
         let newBook = new book(bookID, bookTitle, bookAuthor, bookPages, bookStatus);
-        this.mythis.push(newBook);
+        this.myLibrary.push(newBook);
         localStorage.setItem('myLibrary', JSON.stringify(this.myLibrary)); // convert the array into a string
     },
     deleteBook: (target) => {
-        let item = this.mythis.find(({bookID}) => bookID === parseInt(target.id)); // find method returns first element whose object key bookID matches the buttons id
-        let index = this.mythis.indexOf(item);
+        let item = this.myLibrary.find(({bookID}) => bookID === parseInt(target.id)); // find method returns first element whose object key bookID matches the buttons id
+        let index = this.myLibrary.indexOf(item);
         if (index !== -1){
-            this.mythis.splice(index, 1);
+            this.myLibrary.splice(index, 1);
         }
         localStorage.setItem('myLibrary', JSON.stringify(this.myLibrary)); // push changes to local storage
     },
